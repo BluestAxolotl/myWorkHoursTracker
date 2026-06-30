b2b4aee0a6b07ba72f8240d94f8dea9b49a23015
Alexis Granados <alexis.granados7@proton.me>
2026-06-29 22:42:22 -0500

    Fix type mismatch errors

--- lib/job_profile_graph_section.dart
+++ lib/job_profile_graph_section.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' hide TextDirection;
 
 import 'job_profile.dart';
 import 'job_profile_graph_view_model.dart';
